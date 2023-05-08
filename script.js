4dacd3f7531698b436bbe296d8080dd7fb3e4201
Arnab-1473 <119725146+Arnab-1473@users.noreply.github.com>
2023-05-08 22:43:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// get the DOM elements
+const status = document.getElementById("status");
+const enterBtn = document.getElementById("enterBtn");
+
+// add a click event listener to the button
+enterBtn.addEventListener("click", () => {
+  // create a new h1 element
+  const heading = document.createElement("h1");
+  // set the text of the heading
+  heading.textContent = "Entered Metaverse";
+  // replace the existing p element with the new heading element
+  status.parentNode.replaceChild(heading, status);
+});
